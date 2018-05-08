43fcb2c6592dee66f9f1b839ea1f8fde03174591
Christopher Barfod <christopherbarfod@Christophers-MacBook-Pro.local>
2018-05-08 17:28:52 +1000

    syntax error update - charges_controller.rb

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,31 +1,29 @@
 class ChargesController < ApplicationController
-    def new
-    end
+  def new
+  end
     
-    
-    def create
-      # Amount in cents
-      @amount = @product.price*100
-    
-      customer = Stripe::Customer.create(
-        :email => params[:stripeEmail],
-        :source  => params[:stripeToken]
-      )
-    
-      charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => @amount,
-        :description => 'Rails Stripe customer',
-        :currency    => 'aud'
-      )
-    
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to new_charge_path
-    end
-    private
-      def set_item
-        @product = Product.find(params[:product_id])
-      end
-    end
- end 
+  def create
+    # Amount in cents
+    @amount = @product.price*100
+  
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+  
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'aud'
+    )
+  
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+  private
+  def set_item
+    @product = Product.find(params[:product_id])
+  end
+end
